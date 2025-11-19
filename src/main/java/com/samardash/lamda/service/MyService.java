8d5bfbfde157e2fc7db3ece8a783b86cc610bdae
Samar Dash <12dit152@gmail.com>
2025-11-19 21:33:04 +0000

    Added few more tool for mcp server

---
+++ src/main/java/com/samardash/lamda/service/MyService.java
@@ -0,0 +1,19 @@
+package com.samardash.lamda.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class MyService {
+
+    private final RestClient restClient;
+
+    public MyService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.baseUrl("https://samardash.com").build();
+    }
+
+    public String getDetails() {
+        return this.restClient.get().retrieve().body(String.class);
+    }
+
+}
---
+++ src/main/java/com/samardash/lamda/tools/AboutMeTools.java
@@ -0,0 +1,18 @@
+package com.samardash.lamda.tools;
+
+import com.samardash.lamda.service.MyService;
+import lombok.AllArgsConstructor;
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AboutMeTools {
+
+    private final MyService myService;
+
+    @McpTool(name = "about_samar_dash", description = "Get comprehensive information about Samar Dash - answers any question about personal details, professional background, or biographical information")
+    public String aboutMe() {
+        return myService.getDetails();
+    }
+}
